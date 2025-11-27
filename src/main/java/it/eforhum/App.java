b41b9fcd3d4400012756c3d5d92d5496acfc0482
andrearomanato06-arch <andrearomanato06@gmail.com>
2025-11-27 17:56:25 +0100

    email sending test

--- src/main/java/it/eforhum/App.java
+++ src/main/java/it/eforhum/App.java
@@ -1,8 +1,11 @@
 package it.eforhum;
+import com.github.colozzacristian.*;
 /**
  * Hello world!
  */
 public class App {
+
+    SmtpSession
     public static void main(String[] args) {
         
         System.out.println("Hello World!");
--- src/test/java/it/eforhum/AppTest.java
+++ src/test/java/it/eforhum/AppTest.java
@@ -1,8 +1,14 @@
 package it.eforhum;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import org.junit.Test;
 
+import com.github.colozzacristian.SmtpConnection;
+import com.github.colozzacristian.SmtpConnectionBuilder;
+import com.github.colozzacristian.SmtpResponse;
+import com.github.colozzacristian.SmtpSession;
+
+import io.github.cdimascio.dotenv.Dotenv;
 /**
  * Unit test for simple App.
  */
@@ -12,7 +18,29 @@ public class AppTest {
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void sendsEmail() {
+
+        SmtpConnection c;
+        Integer port = Integer.valueOf(Dotenv.load().get("GMAIL_PORT"));
+
+        String password = Dotenv.load().get("GMAIL_APP_PASSWORD");
+        String user = Dotenv.load().get("GMAIL_ACCOUNT");
+        
+        try {
+            c = SmtpConnectionBuilder.connectSSL("smtp.gmail.com", port , "localhost");
+            SmtpSession session = c.createSession(user, password);
+
+            SmtpResponse response = session.sendMail(user, user, "TEST", "<a href=\"google.com\">test</a>");
+
+            if(response.getCode() != 250){
+                fail();
+            }
+            
+        } catch (Exception e) {
+            fail();
+        }
+
+
+        
     }
 }
